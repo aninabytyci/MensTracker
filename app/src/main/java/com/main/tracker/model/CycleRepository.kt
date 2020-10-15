c2eba2f886dce8d5602ee2cce2ffe6f0e4833947
bytinbit <meline.sieber@hsr.ch>
2020-10-15 10:49:45 +0200

    connect Database with Repository

--- app/src/main/java/com/main/tracker/model/CycleRepository.kt
+++ app/src/main/java/com/main/tracker/model/CycleRepository.kt
@@ -1,11 +1,16 @@
 package com.main.tracker.model
 
+import android.content.Context
+import androidx.room.Room
+import com.main.tracker.model.storage.CycleDatabase
 import java.time.LocalDate
 import java.util.*
 
 
 object CycleRepository {
     val cycles: LinkedList<Cycle> = LinkedList()
+
+    private var database: CycleDatabase? = null
 
     init {
         
@@ -21,5 +26,32 @@ object CycleRepository {
 
     }
 
-}
+    fun initialize(context: Context) {  // TODO initialized DB
+        database = Room.databaseBuilder(
+            context,
+            CycleDatabase::class.java,
+            "roomcycles.db"
+        ).allowMainThreadQueries().build()
+        if (cycles.size == 0) {  // TODO: change to show image instead of prepopulated db
+            addCycle(Cycle( LocalDate.parse("2020-01-01"), LocalDate.parse("2020-01-01"), LocalDate.parse("2020-01-08")))
+            addCycle(Cycle(LocalDate.parse("2020-02-02"), LocalDate.parse("2020-02-10"), LocalDate.parse("2020-02-17")))
+            addCycle(Cycle(LocalDate.parse("2020-03-03"), LocalDate.parse("2020-03-05"), LocalDate.parse("2020-03-12"))            )
+            addCycle(Cycle(LocalDate.parse("2020-04-01"), null, null))
+        }
+    }
 
+    fun addCycle(cycle: Cycle) {
+        database?.cycleDao()?.insertCycle(cycle)
+    }
+
+    fun updateCycle(cycle: Cycle) {
+        database?.cycleDao()?.updateCycle(cycle)
+    }
+
+
+    fun getCycles(): List<Cycle?>? {
+        return database?.cycleDao()?.getAllCycles()
+    }
+
+
+}
